df5c94e2bfe052d14b44bf50a2b925febc7becae
micka <polmickael3@gmail.com>
2017-12-14 19:00:17 +0100

    ajout des fonctions vide

---
+++ js/app.js
@@ -0,0 +1,57 @@
+// Tableau non trié
+var array = [71, 196, 131, 191, 178, 159, 39, 52, 88, 170, 6, 93, 87, 145, 141, 102, 28, 16, 36, 84, 77, 81, 181, 83, 48, 56, 185, 46, 109, 70, 78, 188, 35, 151, 165, 200, 29, 106, 92, 158, 107, 180, 139, 189, 13, 175, 125, 99, 149, 97, 72, 101, 17, 168, 33, 121, 59, 198, 85, 15, 1, 21, 174, 89, 193, 5, 190, 111, 44, 130, 108, 23, 76, 150, 137, 118, 176, 63, 140, 134, 24, 144, 40, 136, 143, 37, 96, 164, 163, 34, 60, 49, 187, 146, 171, 50, 179, 11, 147, 167];
+
+
+
+
+// Fonction utilisant le tri a bulle
+function triBulle(tableau) {
+    
+    // votre code ici
+    
+}
+
+
+
+// Fonction utilisant le tri par insertion
+function triInsertion(tableau) {
+    
+    // votre code ici
+    
+}
+
+
+
+// Fonction utilisant la recherche dichotomique
+function rechercheDicho(tableau) {
+    
+    //  Votre code ici
+    
+}
+
+
+
+// Fonction calculant la somme des N premier entier
+function sommeEntier(n) {
+    
+    // votre code ici    
+    
+}
+
+
+
+// Fonction utilisant la suite de fibonnacci
+function fibo(n) {
+    
+    // votre code ici    
+    
+}
+
+
+
+
+console.log(triBulle(array));
+console.log(triInsertion(array));
+console.log(rechercheDicho(array));
+console.log(sommeEntier(100));
+console.log(fibo(10));
